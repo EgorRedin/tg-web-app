b78a879cb7c955904329df7e9ffbede3831a1f3e
pravdobol <artur251373@gmail.com>
2024-05-06 00:35:49 +0300

    Update api.py
    with JSON and check <14.1

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
-import jsons
+import json
 from bd12 import queries
 from bd12 import configurable_doc
 import asyncpg
@@ -43,9 +43,19 @@ async def db_conn():
     con = await asyncpg.connect(user='postgres',host='localhost',port='5432',database='postgres',password='postgres')
     return con 
 
-async def db_click(tg_id):
+async def db_click(tg_id,count,time):
     con=db_conn()
-    await con.execute ('''INSERT into balance WHERE tg_id={tg_id} VALUES {$1}''',1) 
+    cur=con.cursor()
+    if count//time<14.1:
+        await cur.execute ('''INSERT into balance WHERE tg_id={tg_id} VALUES {$1}''',1)
+
+async def db_get_data(tg_id):
+    conn=db_conn()
+    cur=conn.cursor()
+    await cur.execute("SELECT balance, automainer  WHERE tg_id=%s ", ( tg_id ) )
+    db = cur.fetchone()
+    print ( db )
+    return db        
 
 @app.get("/")
 async def get():
@@ -58,8 +68,11 @@ async def websocket_endpoint_click(websocket: WebSocket,tg_id:str):
     db_tg_id=tg_id
     while True:
         print('1')
-        data = await websocket.receive_text()
-        if data=='1':
-            await db_click(db_tg_id)
-            await websocket.send_text(f"Message text was: {data}")
+        data = await websocket.receive()#recive_text
+        if data["handler"]=='1':
+            await db_click(db_tg_id,data["count"],data["time"])
+            db = await db_get_data(tg_id)
+            user_balance = db[0]
+            automainer = db[1]
+            await websocket.send(json.stringfy(tg_id,user_balance,automainer))#send text
         #elif data==' ' todo next handlers
