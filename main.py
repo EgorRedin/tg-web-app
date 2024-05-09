5dae6744373229c2a56f06338c0b1efe11cb4627
DonaldGubin <bassboosted111@mail.ru>
2024-05-09 21:58:33 +0300

    fgfg

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from queries import AsyncORM
 import socketio
 import uvicorn
-from fastapi import FastAPI
-
+from fastapi import FastAPI, WebSocket
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+import aioredis
+from routersfile import router as router_operation
+import asyncio
+import redis
 
 app = FastAPI()
 sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='asgi')
 socket_app = socketio.ASGIApp(sio)
 app.mount("/", socket_app)
+app.include_router(router_operation)
 
+@app.on_event("startup")
+async def startup_event():
+    redis = await aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    app.redis.close()
+    await redis.wait_close()
 
 @sio.on("init_user")
 async def connection(sid, data):
@@ -41,6 +57,11 @@ async def handle_clicks(sid, data: dict):
     await sio.emit("get_user", ser_user)
 
 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
 
+# @app.on_event("startup")
+# async def startup_event():
+#     redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+#     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
